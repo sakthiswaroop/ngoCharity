94f67cd8b1b718cf3e764be77c8e797b4d3fd40b
santhapa <san.loggingin@gmail.com>
2015-05-26 16:45:20 +0545

    testing widget

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,4 +1,4 @@
-
+<?php /*
   <footer class="footer">
       <div class="container">
         <div class="row">
@@ -65,3 +65,14 @@
         </div>
       </div>  
   </footer>
+  */ ?>
+
+<?php if ( is_active_sidebar( 'widget_footer' ) ) : ?>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <?php dynamic_sidebar( 'widget_footer' ); ?>
+        </div>
+    </div>
+</footer>
+<?php endif; ?>
