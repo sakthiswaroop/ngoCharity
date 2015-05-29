bad6cc33b70af234e35414f882094416530f22ab
santhapa <san.loggingin@gmail.com>
2015-05-29 15:33:38 +0545

    added featured event countdown on sidebar and dynamic widget bar

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,19 +1,54 @@
-<div class="events">
-          <h2>Events</h2>
-          <div class="event-countdown">
-            <h3>Raesent et neque at leo rutrum fermentum</h3>
-            <div class="counter">
-              <div class="count-down"></div>
-            </div>
-            <p>Integer at lacus sed sem placerat ullamcorper. Ut posuere, orci a viverra pellentesque, enim nisi tempus</p>
-            <ul class="list">
-              <li><span><i class="icon-date"></i>25 - 02 - 2013</span><span>08:00am - 12:00pm</span></li>
-              <li><span><i class="icon-location"></i>Washington, United States</span></li>
-            </ul>
-          
-          </div><!-- event-countdown ends -->
-        </div><!-- events (countdonw ends -->
-        <div class="widget-facebook">
-          <h2>Facebook</h2>
-          <div class="fb-like-box" data-href="https://www.facebook.com/themebazaar" data-width="270" data-show-faces="true" data-stream="false" data-show-border="true" data-header="true"></div>
-        </div><!-- widget-facebook ends -->
+<!-- Custom code for featured event -->
+<?php 
+    global $ngoCharity_options;
+    $ngoCharity_settings = get_option( 'ngoCharity_options', $ngoCharity_options );
+    $cat_event = $ngoCharity_settings['event_cat'];
+    query_posts('posts_per_page=1&meta_key=ngoCharity_event_featured&meta_value=1&order=DESC&cat='.$cat_event);
+    if ( have_posts() ) : 
+    ?>
+    <div class="events">
+        <h2>Upcoming Event</h2>
+        <?php while ( have_posts() ) : the_post();
+            $event_datetime = get_post_meta( get_the_ID(), 'ngoCharity_event_datetime', true );
+            $dt = str_replace(array(' ', '|'), '', $event_datetime);
+            
+            date_default_timezone_set('Asia/Kathmandu');
+            $datetime = new DateTime($dt);
+            $ddt = $datetime->getTimestamp();
+        ?>
+            <div class="event-countdown">
+                <h3><?php the_title(); ?></h3>
+                <div class="counter">
+                    <div class="count-down"></div>
+                </div>
+                <p><?php echo ngoCharity_excerpt(get_the_content() , 100); ?></p>
+                <ul class="list"><?php ngoCharity_event_post_meta(); ?></ul>
+            </div><!-- event-countdown ends -->
+        <?php endwhile;
+        ?>
+    </div><!-- events-->
+    <?php 
+    endif;
+    wp_reset_query();   
+?>
+
+
+<script type="text/javascript">
+    jQuery(document).ready(function(e) {
+        "use strict";
+        var counter = "<?php echo $ddt; ?>";
+
+        if (e(".count-down").length !== 0) {
+            e(".count-down").countdown({
+                // timestamp: (new Date).getTime() + 10 * 24 * 60 * 60 * 1e3
+                timestamp:  counter * 1e3
+            })
+        }
+    })
+</script>
+
+<!-- dynamic widget based on  widget bar -->
+
+<?php if ( is_active_sidebar( 'widget_right' ) ) : ?>
+  <?php dynamic_sidebar( 'widget_right' ); ?>
+<?php endif; ?>
