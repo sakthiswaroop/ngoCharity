6b07cdf2409cbc4c5c00a61d3b4044b6d54d2130
santhapa <san.loggingin@gmail.com>
2015-06-24 22:30:00 +0545

    dynamic title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title><?php bloginfo('name'); ?> : <?php bloginfo('description'); ?></title>
+    <title><?php bloginfo('name'); ?><?php wp_title('::'); ?></title>
     <meta name="viewport" content="width=device-width, maximum-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
